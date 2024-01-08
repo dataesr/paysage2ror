347a40ea350bdd47ba781f5ce5a76c8b99a4302c
Anne L'Hôte <anne.lhote@gmail.com>
2024-01-08 15:21:12 +0100

    Fix querying RoR API

--- paysage2ror.mjs
+++ paysage2ror.mjs
@@ -1,10 +1,9 @@
-//  node --experimental-specifier-resolution=node --env-file=.env paysage2ror.mjs
+//  node --experimental-specifier-resolution=node paysage2ror.mjs
 import axios from 'axios';
 import axiosRetry from 'axios-retry';
 import { createObjectCsvWriter } from 'csv-writer';
 import 'dotenv/config';
 
-const PAYSAGE_PAGE_LIMIT = 10; // Set to 0 for no limit
 const PAYSAGE_PAGE_SIZE = 200;
 
 axiosRetry(axios, {
@@ -27,39 +26,45 @@ const getStructuresWithoutRor = async (page = 1) => {
       structures.push({
         paysageId: structure.id,
         paysageUrl: `https://paysage.enseignementsup-recherche.gouv.fr/structures/${structure.id}/presentation`,
-        paysageNames: [structure?.displayName, structure?.currentName?.officialName, structure?.currentName?.usualName].filter((x) => x),
+        paysageNames: [...new Set([structure?.displayName, structure?.currentName?.officialName, structure?.currentName?.usualName].filter((x) => x))],
       })
     }
   });
-  if ((PAYSAGE_PAGE_LIMIT === 0 || page < PAYSAGE_PAGE_LIMIT) && (response.data.data.length === PAYSAGE_PAGE_SIZE)) {
-    const str = await getStructuresWithoutRor(page + 1);
-    structures = [...structures, ...str];
+  if (response.data.data.length === PAYSAGE_PAGE_SIZE) {
+    const newStructures = await getStructuresWithoutRor(page + 1);
+    structures = [...structures, ...newStructures];
   }
   return structures;
 };
 
-const getRors = (structures) => {
-  return Promise.all(structures.map(async (structure) => {
-    for (let i = 0; i < structure.paysageNames.length; i++) {
-      const query = structure.paysageNames[i];
-      await new Promise(resolve => setTimeout(resolve, 30 * 1000));
+const getRors = async (structures) => {
+  const rorStructures = []
+  for (let i = 0; i < structures.length; i++) {
+    const structure = structures[i];
+    for (let j = 0; j < structure.paysageNames.length; j++) {
+      const query = structure.paysageNames[j];
       const response = await axios.get(`https://api.ror.org/organizations?affiliation=${encodeURI(query)}`);
       const result = (response?.data?.items ?? []).find((item) => item?.chosen);
       if (result) {
         structure.ror = result?.organization?.id;
         structure.rorName = result?.organization?.name;
-        return structure;
+        rorStructures.push(structure);
+        break;
       };
     };
-    return structure;
-  }));
+  };
+  return rorStructures;
 };
-const writeCsv = (data) => {
 
+const writeCsv = (data) => {
   const csvWriter = createObjectCsvWriter({
-    path: 'output.csv',
+    path: 'paysage2ror.csv',
     header: [
       { id: 'paysageId', title: 'ID de la structure' },
+      { id: 'paysageUrl', title: 'URL Paysage' },
+      { id: 'paysageNames', title: 'Noms Paysage' },
+      { id: 'ror', title: 'RoR' },
+      { id: 'rorName', title: 'Noms RoR' },
       { id: 'type', title: "Type d'identifiant" },
       { id: 'header1', title: 'Valeur' },
       { id: 'header2', title: 'Date de début {2020-07-02}' },
@@ -70,9 +75,9 @@ const writeCsv = (data) => {
 
   const modifiedData = data.map((record) => {
     return {
-      paysageId: record.paysageId,
+      ...record,
       type: 'ror',
-      header1: record.ror.slice(-9), // 9 last characters of the string 
+      header1: record.ror.replace('https://ror.org/', ''),
       header2: '', // Empty
       header3: '', // Empty
       header4: 'O', // O, because all of them are supposed to be activ
@@ -84,13 +89,13 @@ const writeCsv = (data) => {
 
 
 // Collect all French structures from Paysage without RoR identifier
-console.log('01 _ Collect structures from Paysage')
+console.log('01 _ Collect structures from Paysage');
 const structuresWithoutRor = await getStructuresWithoutRor();
 // Check with our Affiliation Matcher if there is a RoR
-console.log('02 _ For structures, guess RoR from RoR API')
+console.log('02 _ For structures, guess RoR from RoR API');
 const structures = await getRors(structuresWithoutRor);
 const structuresConsolidatedWithRor = structures.filter((structure) => structure?.ror);
 // Write results into CSV
-console.log('03 _ Write results in CSV')
+console.log('03 _ Write results in CSV');
 await writeCsv(structuresConsolidatedWithRor);
 console.log('Done ! Output ready for bulk import !');
